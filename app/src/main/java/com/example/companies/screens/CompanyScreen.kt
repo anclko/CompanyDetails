3cd3fd498995d631b8460d3c3a7c02b440b48964
anclko <ackoch@hotmail.com>
2024-09-14 17:36:05 +0200

    finalise visuals for cards

--- app/src/main/java/com/example/companies/screens/CompanyScreen.kt
+++ app/src/main/java/com/example/companies/screens/CompanyScreen.kt
@@ -7,13 +7,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.example.companies.viewmodel.MainViewModel
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.companies.data.model.Company
 
@@ -42,7 +50,7 @@ fun CompanyScreen(viewModel: MainViewModel = viewModel()){
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(top = 64.dp, start = 16.dp, end = 16.dp, bottom = 32.dp),
     ){
         items(state.list){ company ->
             CompanyCard(company = company)
@@ -56,22 +64,43 @@ fun CompanyCard(company: Company){
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(16.dp)
+            .shadow(8.dp),
+        shape = RoundedCornerShape(12.dp),
     ){
         Column(
             modifier = Modifier
                 .padding(8.dp)
         ){
+            //title of company
             Text(
-                text = company.title
+                text = company.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                fontSize = 22.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+            )
+
+            //location
+            Text(
+                text = "Location: ${company.city}"
+            )
+
+            //webpage
+            Text(
+                text = "Webpage:",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray,
             )
 
             Text(
-                text = company.city
-            )
-
-            Text(
-                text = company.webpage
+                text = company.webpage,
+                fontSize = 16.sp,
+                color = Color(0xFF009688),
+                textDecoration = TextDecoration.Underline,
             )
         }
     }
