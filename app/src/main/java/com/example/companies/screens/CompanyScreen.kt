a279e66346c6de0c579f4d3697a329c23a973346
anclko <ackoch@hotmail.com>
2024-09-14 16:45:32 +0200

    implement composable to display the list of companies

--- app/src/main/java/com/example/companies/screens/CompanyScreen.kt
+++ app/src/main/java/com/example/companies/screens/CompanyScreen.kt
@@ -1,13 +1,21 @@
 package com.example.companies.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import com.example.companies.viewmodel.MainViewModel
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.companies.data.model.Company
 
 @Composable
 fun CompanyScreen(viewModel: MainViewModel = viewModel()){
@@ -30,7 +38,41 @@ fun CompanyScreen(viewModel: MainViewModel = viewModel()){
         }
     }
 
-    //handles successful retrieval
+    //handles successful retrieval and presents the list
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ){
+        items(state.list){ company ->
+            CompanyCard(company = company)
+        }
+    }
 }
 
 //function that formats the list presented
+@Composable
+fun CompanyCard(company: Company){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ){
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ){
+            Text(
+                text = company.title
+            )
+
+            Text(
+                text = company.city
+            )
+
+            Text(
+                text = company.webpage
+            )
+        }
+    }
+}
